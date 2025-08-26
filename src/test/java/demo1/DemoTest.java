375684b75c55d6e60f2aa2d0e5eb3960bdea9717
Ajit Kumar Tung <ajitkumartung7588@gmail.com>
2025-08-26 22:14:03 +0530

    changes

--- src/test/java/demo1/DemoTest.java
+++ src/test/java/demo1/DemoTest.java
@@ -1,5 +1,9 @@
 package demo1;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -10,38 +14,48 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class DemoTest 
 {
 	public static WebDriver driver;
-  @Test(priority=1)
-  public void fun1() 
-  {
-	  WebDriverManager.chromedriver().browserVersion("139").setup();
-	  driver=new ChromeDriver();
-	  
-	  driver.get("https://www.google.com");
-  }
-  
-  @Test(dependsOnMethods="fun1",priority=2)
-  public void fin2()
-  {
-	  driver.manage().window().maximize();
-  }
-  
-  
-  @AfterClass
-  public void tearDown()
-  {
-	  driver.quit();
-	  
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+	@Test(priority=1)
+	public void fun1() 
+	{
+		WebDriverManager.chromedriver().browserVersion("139").setup();
+		driver=new ChromeDriver();
+
+		driver.get("https://www.google.com");
+	}
+
+	@Test(dependsOnMethods="fun1",priority=2)
+	public void fun2()
+	{
+		driver.manage().window().maximize();
+	}
+
+	@Test(priority=3)
+	public void fun3()
+	{
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.findElement(By.xpath("//*[@id='APjFqb']")).sendKeys("ajit",Keys.ENTER);
+
+	}
+
+
+	//  @AfterClass
+	//  public void tearDown()
+	//  {
+	//	  driver.quit();
+	//	  
+	// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
