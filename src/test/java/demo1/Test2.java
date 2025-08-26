d41f7dd87669e2c80dc55859d5a28fb1963f54b4
Ajit Kumar Tung <ajitkumartung7588@gmail.com>
2025-08-27 00:09:25 +0530

    add my new class

---
+++ src/test/java/demo1/Test2.java
@@ -0,0 +1,17 @@
+package demo1;
+
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+  @Test
+  public void f() 
+  {
+	  System.out.println("Hello");
+  }
+  @Test
+  public void g()
+  {
+	  System.out.println("ajit");
+  }
+}
